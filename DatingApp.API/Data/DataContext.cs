df97c899aecbf1ae7e7696d81cef0e7d63f1d576
matrixmagic <matrix.xxcs102@gmail.com>
2018-10-31 23:17:46 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext :DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        public DbSet <Value> Myproperty{get;set;}
+    }
+}
